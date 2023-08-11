0d1292caad08be5bc7bfbfabca6ee8449693fcf6
Samantha Chong <119349070+SamChong015@users.noreply.github.com>
2023-08-11 14:56:34 -0400

    Started programming simple logic.

--- PasswordMaker.h
+++ PasswordMaker.h
@@ -4,60 +4,23 @@
 #include <iostream>
 #include <functional>
 #include <map>
+#include <ctype.h>
 
-std::string password;
-
-bool char8()
+std::string updatePass(std::string password)
 {
-
+	std::cout << "Enter your password ";
+	std::cin >> password;
+	return password;
 }
 
-bool specialchar()
+bool specialChar(const char* char_array)
 {
-
-}
-
-bool hasSubway()
-{
-
-}
-
-bool digits10()
-{
-
-}
-
-bool digitsAdd()
-{
-
-}
-
-bool jakeRhyme()
-{
-
-}
-
-bool specialchar5()
-{
-
-}
-
-bool engSchool()
-{
-
-}
-
-bool colour()
-{
-
-}
-
-bool charlim()
-{
-
-}
-
-
+	for(int i = 0; i < char_array.length(); i++)
+	{
+		if(isalpha(char_array[i]) && char_array[i] !< 0 && char_array[i] !>9)
+			return true;
+	}
+} 
 
 void PasswordMaker()
 {
@@ -67,17 +30,25 @@ void PasswordMaker()
 	passwordRules["Must have at least one special character"] = specialchar;
 	passwordRules["Must have a country that Subway is in"] = hasSubway;
 	passwordRules["Must have exactly 10 digits"] = digits10;
-	passwordRules["Digits must add to 45"] = digitsAdd;
+	passwordRules["Digits must add to 45"] = ;
 	passwordRules["Have a word that rhymes with Jake"] = jakeRhyme;
 	passwordRules["Every fifth character must be a special character"] = specialchar5;
 	passwordRules["Include the name of the best Engineering school in Canada"] = engSchool;
 	passwordRules["Must include a colour"] = colour;
 	passwordRules["Can't have more than x of any character"] = charlim;
 
+	char alphabet[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 
+	while(char_array.length() < 8 && specialChar(char_array) &&)
+		if(char_array.length() < 8)
+			std::cout << "Must have at least 8 characters." << std::endl;
+		else if(specialChar(char_array))
+			std::cout << "Must have at least one special character." <<std::endl;
+
+	const char* char_array = input.c_str();
 
 	std::cout << "Welcome to Password Maker, have fun :)" << std::endl;
-
+	std::string password;
 	system("pause");
 
 	std::cout << "Create your password" << std::endl;
