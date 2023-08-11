727aa6a05d80585d309b751806946eae4256aeff
Samantha Chong <Samantha.chong@soth.ca>
2023-08-11 12:11:39 -0400

    Added Error handling for upper/lowercase text

--- PalindromeChecker.h
+++ PalindromeChecker.h
@@ -9,16 +9,21 @@
 
 bool isPalindrome(std::string input)
 {
+    //makes all lowercase
+    for (char& c : input) {
+        c = std::tolower(c);
+    }
 
+    //turns into array
     const char* char_array = input.c_str();
 
     //removes spaces
     input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
 
-
     //Itterates through chars, exits while loop if encounters a non palindrome
     bool Palindrome = true;
     int i = 0;
+
     while (Palindrome && i < input.length() / 2)
     {
         if (char_array[i] != char_array[(input.length() - 1) - i])
@@ -31,9 +36,11 @@ bool isPalindrome(std::string input)
 
 void PalindromeChecker()
 {
+    system("pause");
+
     std::cout << "Enter your word or phrase." << std::endl;
     std::string input;
-    getline(std::cin, input);
+    std::cin >> input;
 
     std::cout << input << " is ";
     if (isPalindrome(input) == true)
