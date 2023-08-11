1779ecdfc521d8fc6c2dedc83d02c7641e6e61f5
Samantha Chong <Samantha.chong@soth.ca>
2023-08-11 12:11:59 -0400

    Added a dictionary so multiple projects can be tested without terminating the program.

--- Practice Projects.cpp
+++ Practice Projects.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <functional>
+#include <map>
+#include <cstdlib>
 
 #include "RussianRoulette.h"
 #include "PalindromeChecker.h"
@@ -32,7 +34,56 @@ void continueRunning(const std::function<void()>& func)
         }
     }
 }
+
 int main()
 {
-    continueRunning(Lettero);
+    std::map<std::string, std::function<void()>> projectDictionary;
+
+    projectDictionary["RussianRoulette"] = RussianRoulette;
+    projectDictionary["PalindromeChecker"] = PalindromeChecker;
+    projectDictionary["Lettero"] = Lettero;
+
+    std::cout << "Welcome to a compilation of all the projects I have made while slightly bored at work!" << std::endl;
+
+
+    bool running = true;
+    while (running)
+    {
+        std::cout << "Keys in the dictionary:" << std::endl;
+        for (const auto& pair : projectDictionary) {
+            std::cout << pair.first << std::endl;
+        }
+
+        std::cout << "What project do you want to test?" << std::endl;
+        std::string key;
+        std::cin >> key;
+
+        system("cls");
+
+        if (projectDictionary.find(key) != projectDictionary.end()) {
+            continueRunning(projectDictionary[key]); // Call the function associated with the key
+        }
+        else {
+            std::cout << "Function not found" << std::endl;
+        }
+
+        bool continuing = true;
+        while (continuing)
+        {
+            std::cout << "Do you want to pick another project? N will terminate the program. Y/N" << std::endl;
+            std::string input;
+            std::cin >> input;
+
+            if (input == "N" || input == "n")
+            {
+                running = false;
+                continuing = false;
+            }
+            else if (input == "Y" || input == "y")
+            {
+                running = true;
+                continuing = false;
+            }
+        }
+    }
 }
