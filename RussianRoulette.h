8de3ef348f8ae6e6ade2a84b35b104debb40980a
Samantha Chong <Samantha.chong@soth.ca>
2023-08-11 11:05:30 -0400

    Got reading from file working, Russian Roulette is not complete.

--- RussianRoulette.h
+++ RussianRoulette.h
@@ -6,30 +6,62 @@
 #include <time.h> 
 #include <fstream>
 #include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 void RussianRoulette()
 {
-    std::cout << "Are you ready to play Russian Roulette?" << std::endl;
-    system("pause");
-
-    int roulett = rand() % 6 + 1;
-
-    std::cout << "Number is " << roulett << std::endl;
-
-    if (roulett == 6)
+    bool running = true;
+    while (running)
     {
-        std::ifstream file("RussianRoulette"); // Open the file
-        if (!file.is_open()) {
-            std::cerr << "Error opening file: RussianRoulette.txt" << std::endl;
-            return;
-        }
+        std::cout << "Are you ready to play Russian Roulette?" << std::endl;
+        system("pause");
 
-        std::string line;
-        while (std::getline(file, line)) { // Read each line from the file
-            std::cout << line << std::endl; // Write the line to the console
-        }
+        srand(time(NULL));
 
-        file.close(); // Close the file
+        int roulett = rand() % 6 + 1;
+
+        if (roulett == 6)
+        {
+            std::ifstream file("RussianRoulette.txt"); // Open the file
+            if (!file.is_open()) {
+                std::cerr << "Error opening file: RussianRoulette.txt" << std::endl;
+                return;
+            }
+
+            std::string line;
+            while (std::getline(file, line)) { // Read each line from the file
+                std::cout << line << std::endl; // Write the line to the console
+                std::chrono::milliseconds duration(1000);
+                std::this_thread::sleep_for(duration);
+            }
+
+            file.close(); // Close the file
+        }
+        else
+        {
+            std::cout << "You win this time... ";
+        }
+        
+        bool continuing = true;
+        while (continuing)
+        {
+            std::cout << "Do you want to continue? Y/N" << std::endl;
+            std::string input;
+            std::cin >> input;
+
+            if (input == "N" || input == "n")
+            {
+                running = false;
+                continuing = false;
+            }
+            else if (input == "Y" || input == "y")
+            {
+                running = true;
+                continuing = false;
+            }
+        }
     }
 }
 
